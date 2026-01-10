8baed259148cc6172257f7a9af623c50f3b1c652
Mr. Z <mrz1836@users.noreply.github.com>
2026-01-10 17:09:43 -0500

    debug: add TestMain to engine package for goroutine tracing

---
+++ engine/testmain_test.go
@@ -0,0 +1,35 @@
+package engine
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Printf("[TestMain:engine] Starting tests - goroutines: %d\n", runtime.NumGoroutine())
+
+	code := m.Run()
+
+	fmt.Printf("[TestMain:engine] Tests completed with code %d - goroutines: %d\n", code, runtime.NumGoroutine())
+
+	// Wait briefly and check goroutine count again
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("[TestMain:engine] After 100ms - goroutines: %d\n", runtime.NumGoroutine())
+
+	// Give more time for cleanup
+	time.Sleep(500 * time.Millisecond)
+	fmt.Printf("[TestMain:engine] After 600ms - goroutines: %d\n", runtime.NumGoroutine())
+
+	// Print goroutine stacktraces if there are more than expected
+	if runtime.NumGoroutine() > 5 {
+		fmt.Println("[TestMain:engine] WARNING: More goroutines than expected, dumping stacks:")
+		buf := make([]byte, 1024*1024)
+		n := runtime.Stack(buf, true)
+		fmt.Printf("%s\n", buf[:n])
+	}
+
+	os.Exit(code)
+}
